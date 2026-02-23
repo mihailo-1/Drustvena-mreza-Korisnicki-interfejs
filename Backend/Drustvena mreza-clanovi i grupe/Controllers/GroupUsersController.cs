b520d55eba55c30600896aea1a58df744e2ce661
Mihailo <mihakorni@gmail.com>
2026-02-23 14:51:47 +0100

    Inicijalna struktura projekta

---
+++ Backend/Drustvena mreza-clanovi i grupe/Controllers/GroupUsersController.cs
@@ -0,0 +1,83 @@
+﻿using Drustvena_mreza_clanovi_i_grupe.Models;
+using Drustvena_mreza_clanovi_i_grupe.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Drustvena_mreza_clanovi_i_grupe.Controllers
+{
+    [Route("api/groups/{groupId}/users")]
+    [ApiController]
+    public class GroupUsersController : ControllerBase
+    {
+        private UserRepository userRepository = new UserRepository();
+        private GroupRepository groupRepository = new GroupRepository();
+        private GroupUsersRepository groupUsersRepository = new GroupUsersRepository();
+
+        [HttpGet]
+        public ActionResult<List<User>> GetUsersByGroup(int groupId)
+        {
+            if (!GroupRepository.Data.ContainsKey(groupId))
+            {
+                return NotFound();
+            }
+
+            Group group = GroupRepository.Data[groupId];
+            return Ok(group.Korisnici);
+        }
+
+        // B.2
+        [HttpPut("{userId}")]
+        public ActionResult AddUserToGroup(int groupId, int userId)
+        {
+            if (!GroupRepository.Data.ContainsKey(groupId) || !UserRepository.Data.ContainsKey(userId))
+            {
+                return NotFound("Grupa ili korisnik ne postoje.");
+            }
+
+            var group = GroupRepository.Data[groupId];
+            var user = UserRepository.Data[userId];
+
+            if (!group.Korisnici.Any(u => u.Id == userId))
+            {
+                group.Korisnici.Add(user);
+
+                if (!GroupUsersRepository.Data.ContainsKey(groupId))
+                    GroupUsersRepository.Data[groupId] = new List<int>();
+
+                GroupUsersRepository.Data[groupId].Add(userId);
+
+                groupUsersRepository.Save();
+            }
+
+            return Ok();
+        }
+
+        // B.2
+        [HttpDelete("{userId}")]
+        public ActionResult RemoveUserFromGroup(int groupId, int userId)
+        {
+            if (!GroupRepository.Data.ContainsKey(groupId))
+            {
+                return NotFound("Grupa ne postoji.");
+            }
+
+            var group = GroupRepository.Data[groupId];
+            var userForRemoval = group.Korisnici.FirstOrDefault(u => u.Id == userId);
+
+            if (userForRemoval != null)
+            {
+                group.Korisnici.Remove(userForRemoval);
+
+                if (GroupUsersRepository.Data.ContainsKey(groupId))
+                {
+                    GroupUsersRepository.Data[groupId].Remove(userId);
+                }
+
+                groupUsersRepository.Save();
+                return NoContent();
+            }
+
+            return NotFound("Korisnik nije član ove grupe.");
+        }
+    }
+}
---
+++ Backend/Drustvena mreza-clanovi i grupe/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿namespace Drustvena_mreza_clanovi_i_grupe.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public DateTime DatumOsnivanja { get; set; }
+        public List<User> Korisnici { get; set; } = new List<User>();
+
+        public Group(int id, string ime, DateTime datumOsnivanja)
+        {
+            Id = id;
+            Ime = ime;
+            DatumOsnivanja = datumOsnivanja;
+        }
+
+        public Group() { }
+    }
+}
